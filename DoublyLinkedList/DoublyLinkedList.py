f85ac20c3f994cddecabcf7cc1ba6314578666cd
themorsten <75784716+themorsten@users.noreply.github.com>
2023-08-21 13:25:38 +0300

    Create DoublyLinkedList.py

---
+++ DoublyLinkedList/DoublyLinkedList.py
@@ -0,0 +1,34 @@
+class DoublyLinkedListNode:
+  def __init__(self,value):
+    self.value = value
+    self.next = None
+    self.prev = None
+
+
+class DoublyLinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+
+  def append(self, value):
+    newNode = DoublyLinkedListNode(value)
+
+    if self.head: # список непустой
+      self.tail.next = newNode
+      newNode.prev = self.tail
+      self.tail = newNode
+
+    else: # список пустой
+      self.head = newNode
+      self.tail = newNode
+
+  def prepend(self, value):
+    newNode = DoublyLinkedListNode(value)
+
+    if self.head: # список непустой
+      self.head.prev = newNode
+      newNode.next = self.head
+      self.head = newNode
+    else:
+      self.head = newNode
+      self.tail = newNode
