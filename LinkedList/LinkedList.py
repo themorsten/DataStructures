841f28cb30abdeb36fdeea301933846a3852e6be
themorsten <75784716+themorsten@users.noreply.github.com>
2023-08-21 13:24:27 +0300

    Update LinkedList.py

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,14 +1,14 @@
-class LinkedListNode:
+class LinkedListNode: # звено
   def __init__ (self,value):
     self.value = value
     self.next = None
 
-class LinkedList:
+class LinkedList: # связный список
   def __init__(self):
     self.head = None
     self.tail = None
 
-  def append(self,value):
+  def append(self,value): # добавить в конец 
     newNode = LinkedListNode(value) # создаем звено
 
     if self.head: # список непустой 
@@ -19,7 +19,7 @@ class LinkedList:
       self.head = newNode
       self.tail = newNode
 
-  def prepend(self, value):
+  def prepend(self, value): # добавить в начало 
     newNode = LinkedListNode(value)
 
     if self.head: # список непустой
